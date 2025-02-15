bfe6b7ce962ea40d81c50ac6df8524824b346572
Savelkins <savelkains@gmail.com>
2025-02-16 01:04:01 +0200

    everything from the beginning

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import styles from "./App.module.scss";
-import SideBar from "./components/SideBar/SideBar";
-import BouncingBalls from "./components/BonchingBalls/BoncingBalls";
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className={styles.flex}>
-        <SideBar />
-        <BouncingBalls />
         <Routes>
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<ProfilePage />} />
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
